95fe40c6dee00400440e482cac57147198af6bcd
gustavo1371 <gustavodarochasilvarocha@gmail.com>
2025-06-08 17:24:22 -0300

    Foi feito a criação do repositório no GitHub; Foi feito a conexão do projeto; Foi adicionado os 6 Forms necessários para o trabalho, além dos botões no primeiro Forms;

---
+++ SistemaDCadastro/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SistemaDCadastro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnEntrar = new Button();
+            lblUsuario = new Label();
+            lblSenha = new Label();
+            txtUsuario = new TextBox();
+            txtSenha = new TextBox();
+            SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.Location = new Point(152, 200);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(117, 46);
+            btnEntrar.TabIndex = 0;
+            btnEntrar.Text = "Entrar";
+            btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(12, 9);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(53, 15);
+            lblUsuario.TabIndex = 1;
+            lblUsuario.Text = "Usuário: ";
+            // 
+            // lblSenha
+            // 
+            lblSenha.AutoSize = true;
+            lblSenha.Location = new Point(12, 89);
+            lblSenha.Name = "lblSenha";
+            lblSenha.Size = new Size(48, 15);
+            lblSenha.TabIndex = 2;
+            lblSenha.Text = " Senha: ";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(12, 27);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(257, 23);
+            txtUsuario.TabIndex = 3;
+            // 
+            // txtSenha
+            // 
+            txtSenha.Location = new Point(12, 107);
+            txtSenha.Name = "txtSenha";
+            txtSenha.Size = new Size(257, 23);
+            txtSenha.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(281, 258);
+            Controls.Add(txtSenha);
+            Controls.Add(txtUsuario);
+            Controls.Add(lblSenha);
+            Controls.Add(lblUsuario);
+            Controls.Add(btnEntrar);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnEntrar;
+        private Label lblUsuario;
+        private Label lblSenha;
+        private TextBox txtUsuario;
+        private TextBox txtSenha;
+    }
+}
---
+++ SistemaDCadastro/Form1.cs
@@ -0,0 +1,10 @@
+namespace SistemaDCadastro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
